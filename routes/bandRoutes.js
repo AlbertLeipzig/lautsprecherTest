f17a4573e2b14454eb8bdd14b6e6ad25f183c8e8
AlbertLeipzig <albertclemvill@gmail.com>
2023-01-07 11:11:17 +0100

    cyclic 6

--- routes/bandRoutes.js
+++ routes/bandRoutes.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const {
+const getAllBands = require('../controllers/bandController');
+const getSingleBand = require('../controllers/bandController');
+const addBand = require('../controllers/bandController');
+const updateBand = require('../controllers/bandController');
+const deleteBand = require('../controllers/bandController');
+
+/* const {
   getAllBands,
   getSingleBand,
   addBand,
   updateBand,
   deleteBand,
-} = require('../controllers/BandController.js');
+} = require('../controllers/BandController.js'); */
 
 router.route('/').get(getAllBands).post(addBand);
 router.route('/:id').get(getSingleBand).patch(updateBand).delete(deleteBand);
